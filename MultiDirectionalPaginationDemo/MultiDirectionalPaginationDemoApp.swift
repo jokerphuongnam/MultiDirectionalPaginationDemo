839576b7746f4da9a305cd909c34558d20b1e23e
Phương Nam <nampp@mapmaker.asia>
2022-06-10 09:27:54 +0700

    Initial Commit

---
+++ MultiDirectionalPaginationDemo/MultiDirectionalPaginationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiDirectionalPaginationDemoApp.swift
+//  MultiDirectionalPaginationDemo
+//
+//  Created by gannha on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiDirectionalPaginationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
